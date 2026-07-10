f24881cc0282aa90a97c2ddecc19863a507b911e
avelasquezh <arleyvelasquez29@gmail.com>
2026-07-10 00:02:36 -0500

    feat: initialize Karate automation framework

---
+++ src/test/java/com/portfolio/posts/PostsRunner.java
@@ -0,0 +1,11 @@
+package com.portfolio.posts;
+
+import com.intuit.karate.junit5.Karate;
+
+class PostsRunner {
+
+    @Karate.Test
+    Karate testPosts() {
+        return Karate.run("posts").relativeTo(getClass());
+    }
+}   
